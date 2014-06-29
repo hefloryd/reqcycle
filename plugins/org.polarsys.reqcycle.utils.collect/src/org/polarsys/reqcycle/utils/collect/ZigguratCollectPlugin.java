21b95b68e53d85fbe084aa7bae86bd4a56bb556e
Anass RADOUANI <anass.radouani@atos.net>
2014-06-29 16:48:36 +0200

    Added a functionality to easily compute dependencies and list them according to their depth, provided a starting element and a picker.

--- plugins/org.polarsys.reqcycle.utils.collect/src/org/polarsys/reqcycle/utils/collect/ZigguratCollectPlugin.java
+++ plugins/org.polarsys.reqcycle.utils.collect/src/org/polarsys/reqcycle/utils/collect/ZigguratCollectPlugin.java
@@ -10,8 +10,14 @@
  *******************************************************************************/
 package org.polarsys.reqcycle.utils.collect;
 
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.polarsys.reqcycle.utils.collect.collectors.DepthMeasuringHarvester;
+import org.polarsys.reqcycle.utils.collect.yieldadapters.ThreadedYieldAdapter;
 
 public class ZigguratCollectPlugin implements BundleActivator {
 
@@ -35,6 +41,27 @@ public class ZigguratCollectPlugin implements BundleActivator {
 	 */
 	public void stop(BundleContext bundleContext) throws Exception {
 		ZigguratCollectPlugin.context = null;
+	}
+	
+	/**
+	 * Method used to get to create a dependency list out of pickers. A DepthMeasuringHarvester 
+	 * is used to iterate other elements, and the depth is computed while the elements are iterated on.
+	 * In the end, a list of dependencies is retrieved from the harvester. 
+	 * 
+	 * if A.getNext() = [B,C,D] then B,C,D are deeper than A and A depends on B,C,D.
+	 * 
+	 * @return a list going from the deepest element, to the most shallow.
+	 */
+	public static <T> List <T> getDependencyList(T startingElement, Picker<T> picker){
+		@SuppressWarnings("unchecked")
+		DepthMeasuringHarvester<T> harvester =  new DepthMeasuringHarvester<T>(startingElement, Collections.singleton(picker));
+		Iterable<T> iterable = new ThreadedYieldAdapter<T>().adapt(harvester);
+		Iterator<T> iterator = iterable.iterator();
+		while (iterator.hasNext()){
+			//Do nothing, just going through the iterator
+			iterator.next();
+		}
+		return harvester.getElementsFromDeepest();
 	}
 
 }
---
+++ plugins/org.polarsys.reqcycle.utils.collect/src/org/polarsys/reqcycle/utils/collect/collectors/DepthMeasuringHarvester.java
@@ -0,0 +1,90 @@
+package org.polarsys.reqcycle.utils.collect.collectors;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.polarsys.reqcycle.utils.collect.Picker;
+import org.polarsys.reqcycle.utils.collect.ResultHandler;
+import org.polarsys.reqcycle.utils.collect.exceptions.CannotHandleException;
+import org.polarsys.reqcycle.utils.collect.exceptions.CannotPickException;
+import org.polarsys.reqcycle.utils.collect.exceptions.CollectionAbortedException;
+
+import com.google.common.collect.Lists;
+
+/**
+ * A depth harvester that keeps a trace of the longest depth the element can be found at.
+ */
+public class DepthMeasuringHarvester<T> extends AbstractCollector<T> {
+
+	protected Map<T, Integer> longestPath = new HashMap<T, Integer>();
+
+
+	public DepthMeasuringHarvester(T startingElement, Iterable<? extends Picker<T>> pickers) {
+		super(startingElement, pickers);
+	}
+
+	@Override
+	public void collect(ResultHandler<T> handler) throws CollectionAbortedException {
+		getLongestPathMap().put(start, 0);
+		collectDepthWise(handler, start, 0);
+	}
+
+	public Map<T, Integer> getLongestPathMap() {
+		return longestPath;
+	}
+
+	protected void collectDepthWise(ResultHandler<T> handler, T element, int depth) throws CollectionAbortedException {
+		try {
+			handler.handleResult(element); //send result to the handler.
+			for(Picker<T> picker : this.getPickers()) {
+				Iterable<T> nexts = picker.getNexts(element); //getting children.
+				if(nexts != null) { //some elements do not have any child.
+					for(T next : nexts) {
+
+						Integer nextOldDepth = getLongestPathMap().get(next);
+
+						Integer nextCurrentDepth = Math.max(nextOldDepth != null ? nextOldDepth : -1, depth + 1);
+						if(nextCurrentDepth != nextOldDepth) {
+							getLongestPathMap().put(next, nextCurrentDepth);
+						}
+						this.collectDepthWise(handler, next, nextCurrentDepth); //recurse
+					}
+				}
+			}
+		} catch (CannotHandleException e) {
+			// if the element cannot be handled, do nothing.
+		} catch (CannotPickException e) {
+			throw new CollectionAbortedException(e);
+		}
+	}
+
+	/**
+	 * Gets a list of the elements computed during the iteration process,
+	 * from the deepest to the most shallow.
+	 */
+	public List<T> getElementsFromDeepest() {
+		Set<T> keySet = longestPath.keySet();
+		List<T> result = Lists.newArrayList(keySet);
+		Collections.sort(result, new Comparator<Object>() {
+
+			public int compare(Object arg0, Object arg1) {
+				Integer depth0 = getLongestPathMap().get(arg0);
+				Integer depth1 = getLongestPathMap().get(arg1);
+				if(depth0 > depth1) {
+					return -1;
+				} else if(depth0 == depth1) {
+					return 0;
+				} else {
+					return 1;
+				}
+			}
+		});
+
+		return result;
+	}
+
+}
